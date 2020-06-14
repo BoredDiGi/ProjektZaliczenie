eef3c90509fffbd73c8afd8b5d19faaa1dca7dcc
BoredDiGi <61658897+BoredDiGi@users.noreply.github.com>
2020-06-14 16:00:00 +0200

    ..

---
+++ classlibrary/gameall.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classlibrary
+{
+    public class gameall
+    {
+        public static void gamemode()
+        {
+            Console.Title = "Wisielec the game";
+            string c = "start"; //menu
+            string[] max = System.IO.File.ReadAllLines("../../bestscore.txt"); //best/highscore data
+            string[] texts = System.IO.File.ReadAllLines("../../text.txt"); //text data
+            int newmax = int.Parse(max[0]); //local new variable max
+            int ckmax = 0; //local support variable max
+            int ckrand = texts.Length + 1; //old max support randomizer
+            string[] test = new string[1]; //support highscore system 
+            do
+            {
+                //Console.Clear();*
+                Console.Write("wciśnij cokolwiek aby grać, 0 wyłączy grę :) : ");
+                c = Console.ReadLine(); //menu switch
+                Console.WriteLine("Zagrajmy w grę, najwyższy wynik wynosi: " + newmax + " , a obecny wynik wynosi: " + ckmax);
+                ckrand = lib.randomizer(texts.Length, ckrand); //initialize random engine
+                gameinitialize.gameprinter(texts[ckrand]);
+                if (lib.highscore(test, newmax, ckmax) == true)
+                {
+                    newmax = ckmax; //if true ckmax is newmax
+                }
+            } while (c != "0");
+            lib.end();
+        }
+    }
+}
---
+++ classlibrary/gameinitialize.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace classlibrary
+{
+    public class gameinitialize
+    {
+        public static int gameprinter(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("wylosowany wyraz ma: "+text.Length+" liter");
+            for(int counter = 0; counter < text.Length; counter++)
+            {
+                Console.Write(" _ ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
+

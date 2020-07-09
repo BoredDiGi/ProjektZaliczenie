80e13d0a9c484838fa25e28a1b8fb909440fcc9a
BoredDiGi <61658897+BoredDiGi@users.noreply.github.com>
2020-07-09 16:48:43 +0200

    added last words list

--- classlibrary/Class2.cs
+++ classlibrary/Class2.cs
@@ -10,7 +10,6 @@ namespace classlibrary
     {
         public static void gamemode()
         {
-            Console.Beep();
             Console.Title = "Wisielec the game";
             gameinitialize.opener();
             Console.WriteLine("kliknij przycisk aby zaakceptować");
@@ -19,6 +18,7 @@ namespace classlibrary
             string c = "start"; //menu
             string[] max = System.IO.File.ReadAllLines("../../bestscore.txt"); //best/highscore data
             string[] texts = System.IO.File.ReadAllLines("../../text.txt"); //text data
+            var words = new List<string>();
             int newmax = int.Parse(max[0]); //local new variable max
             int ckmax = 0; //local support variable max
             int ckrand = texts.Length + 1; //old max support randomizer
@@ -28,25 +28,51 @@ namespace classlibrary
             do
             {
                 Console.Clear();
-                Console.Write("wciśnij cokolwiek aby grać, 0 wyłączy grę :) : ");
+                Console.Write(" [1] aby grać \n [2] aby zobaczyć ostatnie słowa \n [0] wyłączy grę \n wybieram: ");
                 c = Console.ReadLine(); //menu switch
-                if(c == "0")
+                if (c == "0")
                 {
                     break;
                 }
-                Console.WriteLine("Zagrajmy w grę, najwyższy wynik wynosi: " + newmax + " , a obecny wynik wynosi: " + ckmax);
-                ckrand = lib.randomizer(texts.Length, ckrand); //initialize random engine
-                if (gameinitialize.gamechk(texts[ckrand]) == true)
+                else if (c == "2")
                 {
-                    ckmax++;
-                    Console.WriteLine("najwyższy wynik: "+ newmax + " aktualny wynik: " + ckmax);
-                    Console.WriteLine("wciśnij dowolny klawisz aby kontynuować");
+                    if (words.Count < 1)
+                    {
+                        Console.WriteLine("brak elementów do wyświetlenia");
+                        Console.WriteLine("Kliknij dowolny klawisz aby kontynuować");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        for (int pp = 0; pp < words.Count; pp++)
+                        Console.WriteLine(words[pp]);
+                        Console.WriteLine("Kliknij dowolny klawisz aby kontynuować");
+                        Console.ReadKey();
+                    }
+                }
+                else if(c != "0" || c != "1" || c != "2")
+                {                  
+                    Console.WriteLine("podano złą wartość");
+                    Console.WriteLine("Kliknij dowolny klawisz aby kontynuować");
                     Console.ReadKey();
-                    if ( newmax < ckmax)//if true ckmax is newmax
-                    { 
-                        newmax = ckmax; 
-                        save[0] = newmax.ToString();
-                        System.IO.File.WriteAllLines("../../bestscore.txt", save);
+                }
+                else if(c == "1")
+                {
+                    Console.WriteLine("Zagrajmy w grę, najwyższy wynik wynosi: " + newmax + " , a obecny wynik wynosi: " + ckmax);
+                    ckrand = lib.randomizer(texts.Length, ckrand); //initialize random engine
+                    words.Add(texts[ckrand]);
+                    if (gameinitialize.gamechk(texts[ckrand]) == true)
+                    {
+                        ckmax++;
+                        Console.WriteLine("najwyższy wynik: " + newmax + " aktualny wynik: " + ckmax);
+                        Console.WriteLine("wciśnij dowolny klawisz aby kontynuować");
+                        Console.ReadKey();
+                        if (newmax < ckmax)//if true ckmax is newmax
+                        {
+                            newmax = ckmax;
+                            save[0] = newmax.ToString();
+                            System.IO.File.WriteAllLines("../../bestscore.txt", save);
+                        }
                     }
                 }
             } while (true);
